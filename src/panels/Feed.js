82c874b450564e3955c3eecea5a904c2a05a435b
Lev Vasilyev <lev@Levs-MacBook-Pro.local>
2018-11-10 06:41:16 +0300

    better feed + utils

--- src/panels/Feed.js
+++ src/panels/Feed.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Panel, PanelHeader, HeaderButton, platform, IOS, Group, Div, Header, Gallery, Spinner, Footer, Cell} from '@vkontakte/vkui';
+import {Panel, PanelHeader, HeaderButton, platform, IOS, Group, Div, Header, Gallery, Spinner, Footer, Cell, Search, HorizontalScroll, Button} from '@vkontakte/vkui';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 import axios from 'axios'
+
+import { tagColor } from './../utils/utils'
 
 const osname = platform();
 
@@ -12,6 +14,9 @@ class Feed extends React.Component {
     super(props)
     this.state = {
       articles: null,
+      value: "",
+      selectedTags: [],
+      popularTags: [],
     }
   }
 
@@ -19,6 +24,26 @@ class Feed extends React.Component {
   componentWillReceiveProps (props) {
     axios.get('/articles.json').then(res => {
       this.setState({articles: res.data});
+      let popularTags = [];
+      res.data.forEach(article => {
+        article.tags.forEach(tag => {
+          let index = popularTags.findIndex(tagg => tagg.tag == tag);
+
+          if (index === -1)
+            popularTags.push({tag: tag, popularity: 1});
+          else
+            popularTags = [
+              ...popularTags.slice(0, index),
+              {tag: tag, popularity: (popularTags[index].popularity + 1)},
+              ...popularTags.slice(index + 1)
+            ];
+        });
+      });
+      popularTags = popularTags
+      .sort((a, b) => b.popularity - a.popularity)
+      .filter(tag => tag.popularity > 1)
+      .map(tag => tag.tag);
+      this.setState({popularTags: popularTags});
     }).catch((e) => {
       this.setState({
         articles: [],
@@ -26,14 +51,77 @@ class Feed extends React.Component {
     })
   }
 
-  renderArticleListing(article) {
+  toggleTag(tag) {
+    let tags = this.state.selectedTags;
+    let index = this.state.selectedTags.indexOf(tag);
+    if (index == -1)
+      tags.push(tag);
+    else
+      tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+    this.setState({selectedTags: tags});
+  }
+
+  renderTags(tags) {
     return (
-      <Group onClick={() => {
-        this.props.setArticle(article.id);
-        this.props.open("article");
-      }}>
+      <HorizontalScroll>
+      {
+        tags.map(tag => {
+          let style = {};
+          if (this.state.selectedTags.includes(tag))
+            style = {
+              margin: "0 5px",
+              background: tagColor(tag),
+              color: "white",
+            };
+          else
+            style = {
+              margin: "0 5px",
+              background: "rgba(1, 1, 1, 0)",
+              color: tagColor(tag),
+              boxShadow: "inset 0px 0px 0px 2px " + tagColor(tag),
+            };
+
+          return (
+            <Button
+              onClick={() => this.toggleTag(tag)}
+              style={style}
+            >
+              #{tag}
+            </Button>
+          );
+        })
+      }
+      </HorizontalScroll>
+    );
+  }
+
+  renderArticleListing(article) {
+    let text = article.article.find(node => node.type == "text") || {text: ""};
+    let pics = article.article.find(node => node.type == "gallery");
+
+    return (
+      <Group>
+        <Div onClick={() => {
+          this.props.setArticle(article.id);
+          this.props.open("article");
+        }}>
+          <h3 style={{margin: "0px 0"}}>{article.title}</h3>
+          {/* {
+            !pics ? "" :
+              <Gallery
+                slideWidth="100%"
+                style={{ height: "auto", width: "100%" }}
+                bullets="dark"
+              >
+                {pics.pics.map(pic => <img src={pic}/>)}
+              </Gallery>
+          } */}
+          {pics.pics.length > 0 ? <img src={pics.pics[0]} style={{width: "100%"}} /> : ""}
+          <p>{text.text}</p>
+        </Div>
         <Cell>
-          {article.title}
+          <small>Теги:</small>
+          {this.renderTags(article.tags)}
         </Cell>
       </Group>
     );
@@ -44,8 +132,18 @@ class Feed extends React.Component {
       return <Div><Spinner /></Div>;
     if (this.state.articles.length == 0)
       return <Footer>нет новостей</Footer>;
-    console.log(this.state.articles);
-    return this.state.articles.map(article => this.renderArticleListing(article));
+    return this.state.articles
+    .filter(article =>
+      (
+        this.state.selectedTags.length == 0 ||
+        this.state.selectedTags.map(tag => article.tags.includes(tag)).reduce((a, b) => a || b)
+      ) && (
+        article.title.includes(this.state.value) ||
+        article.article.map(node => node.type =="text" && node.text.includes(this.state.value)).reduce((a, b) => a || b) ||
+        article.tags.map(tag => tag.includes(this.state.value)).reduce((a, b) => a || b)
+      )
+    )
+    .map(article => this.renderArticleListing(article));
   }
 
   render() {
@@ -54,6 +152,13 @@ class Feed extends React.Component {
         <PanelHeader>
           Новости
         </PanelHeader>
+        <Search theme="default" onChange={value => this.setState({value: value})} />
+        <Div>
+          <small>Популярные теги:</small>
+        </Div>
+        <Cell>
+          {this.renderTags(this.state.popularTags)}
+        </Cell>
         {this.renderArticles()}
       </Panel>
     );
